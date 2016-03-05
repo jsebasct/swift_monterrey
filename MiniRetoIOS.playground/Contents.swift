96792afa6a0637481ac46a4134f79228623589c9
sebastian cruz <jsebas.ct@gmail.com>
2016-03-05 20:08:49 -0300

    Mini Reto iOS - las condiciones no son excluyentes

--- MiniRetoIOS.playground/Contents.swift
+++ MiniRetoIOS.playground/Contents.swift
@@ -8,21 +8,20 @@ var serie = 0...100
 
 
 for num in serie {
-
-    var message = ""
-    
+ 
     if num % 5 == 0 {
-        message = " Bingo!!!"
-    } else if num % 2 == 0 {
-        message = " Par!!!"
-    } else if num % 2 != 0 {
-        message = " Impar!!!"
+        print(num, "Bingo!!!")
+    }
+    
+    if num % 2 == 0 {
+        print(num, "Par!!!")
+    }
+    
+    if num % 2 != 0 {
+        print(num, "Impar!!!")
     }
     
     if num >= 30 && num <= 40 {
-        message = " Viva Swift!!!"
+        print(num, "Viva Swift!!!")
     }
-    
-    print(String(num) + "\(message)")
 }
-
